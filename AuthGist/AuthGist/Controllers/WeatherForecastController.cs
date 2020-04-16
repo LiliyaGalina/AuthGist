72bc9ba188163e47373ea4474bfde11a9ed00cd9
Liliya Galina <liliya.galina@neadevis.com>
2020-04-16 21:51:36 +0300

    Add Swagger

--- AuthGist/AuthGist/Controllers/WeatherForecastController.cs
+++ AuthGist/AuthGist/Controllers/WeatherForecastController.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace AuthGist.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("weather-forecasts")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
